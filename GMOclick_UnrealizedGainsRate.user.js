dadc0fbfc042169c932eaf8d93d645e6db6c0d65
usagi2 <usagi2@users.noreply.github.com>
2015-06-27 17:16:41 +0900

    Chromeで動作するように修正

--- GMOclick_UnrealizedGainsRate.user.js
+++ GMOclick_UnrealizedGainsRate.user.js
@@ -3,11 +3,13 @@
 // @description GMOクリック証券の信用建玉一覧に評価損益率を表示します。(サマリー表示のチェックが付いてると動作しません)
 // @namespace   usagi2
 // @include     https://kabu.click-sec.com/sec1-1/kabu/tatePositionList.do
-// @version     0.2
+// @version     0.3
 // @downloadURL https://github.com/usagi2/userscript/raw/master/GMOclick_UnrealizedGainsRate.user.js
 // @grant       none
 // @run-at      document-end
 // ==/UserScript==
+
+(function () {
 'use strict';
 
 var rows = document.querySelectorAll('tr[class="odd"], tr[class="even"]');
@@ -16,13 +18,13 @@ for each (var row in rows) {
   if (row.cells && row.cells.length == 10) {
     var soneki = parseInt(removeComma(trim(row.cells[8].textContent))); // 損益
     
-    if (soneki == 0) {
-      contnue;
+    if (soneki === 0) {
+      continue;
     }
 
     var c7 = removeComma(trim(row.cells[7].textContent));         // 建玉代金+諸経費
-    var [daikin, other] = c7.replace(/\s+/g, ' ').split(/\s/, 2); // 分離
-    var tatekei = parseInt(daikin) + parseInt(other);             // 合計
+    var tmp = c7.replace(/\s+/g, ' ').split(/\s/, 2); // 分離
+    var tatekei = parseInt(tmp[0]) + parseInt(tmp[1]);             // 合計
     var p = (soneki / tatekei * 100).toFixed(2);                  // 損益率
     
     var span = document.createElement('span');
@@ -40,3 +42,5 @@ function trim(str) {
 function removeComma(str) {
   return str.replace(/,/g, '');
 }
+
+})();
