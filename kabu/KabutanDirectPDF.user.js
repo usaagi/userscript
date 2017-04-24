fb425e61a8313ed7241edb2600b0a9fe34b4b670
usagi2 <usagi2@users.noreply.github.com>
2017-04-24 22:10:58 +0900

    Create KabutanDirectPDF.user.js

---
+++ kabu/KabutanDirectPDF.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name        株探ダイレクトPDF
+// @description 株探内のPDFを従来のように直接表示します。戻るボタンが効かなくなるので新しいタブで開いてください。
+// @namespace   usagi2
+// @include     http://kabutan.jp/disclosures/pdf/*/*/
+// @run-at      document-start
+// @downloadURL https://github.com/usagi2/userscript/raw/master/KabutanDirectPDF.user.js
+// @version     0.1.5
+// ==/UserScript==
+
+'use strict';
+
+location.href = location.href.replace('/kabutan.jp/disclosures/pdf/', '/211.6.211.247/tdnet/data/').replace(/\/$/, '.pdf');
