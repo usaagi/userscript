171f39fccec62a489315ad8795fb03d57f6453eb
Unknown <usagi.com2@gmail.com>
2019-04-19 13:43:27 +0900

    namespaceの変更
    namespaceをusagiからusaagi へ

--- kabu/rakutenTelcon.user.js
+++ kabu/rakutenTelcon.user.js
@@ -1,35 +1,30 @@
 // ==UserScript==
 // @name        楽天
 // @description テレコンを普通のブラウザで利用
-// @namespace   usagi
+// @namespace   usaagi
 // @include     https://member.rakuten-sec.co.jp/app/*.do;BV_SessionID=*
-// @version     0.1
+// @version     0.2
 // @run-at      document-end
 // ==/UserScript==
 
 
-(function () {
-  'use strict';
-  
-  console.log(`テレコンのリンクを表示 ${location.href}`);
-  
-  let m = location.href.match(String.raw`/app/.*\.do;BV_SessionID=(.*)\?eventType=`);
+'use strict';
 
-  if (!m) {
-    return;
-  }
-  
-  let session_id = m[1];
-  
-  console.log(`session id: ${session_id}`);
-//  console.dir(document.getElementById('siteID').getElementsByTagName('a')[0].getAttribute('href'));
+console.log(`テレコンのリンクを表示 ${location.href}`);
+
+let m = location.href.match(String.raw`/app/.*\.do;BV_SessionID=(.*)\?eventType=`);
+
+if (m) {
+
+    let session_id = m[1];
+
+    console.log(`session id: ${session_id}`);
+    //  console.dir(document.getElementById('siteID').getElementsByTagName('a')[0].getAttribute('href'));
        
-  let div = document.createElement('div');
-  div.innerHTML = `<a href="https://member.rakuten-sec.co.jp/bv/app/info_jp_nikkei_telecom.do;BV_SessionID=${session_id}?eventType=init&agreeFlg=0">日経テレコンへ</a>`;
-  div.setAttribute('style', 'padding: 10px;');
+    let div = document.createElement('div');
+    div.innerHTML = `<a href="https://member.rakuten-sec.co.jp/bv/app/info_jp_nikkei_telecom.do;BV_SessionID=${session_id}?eventType=init&agreeFlg=0">日経テレコンへ</a>`;
+    div.setAttribute('style', 'padding: 10px;');
 
-  // 上部に表示させるには insertBefore 下部は appendChild
-  document.body.appendChild(div, document.body.firstChild);
-
-
-})();
+    // 上部に表示させるには insertBefore 下部は appendChild
+    document.body.appendChild(div, document.body.firstChild);
+}
