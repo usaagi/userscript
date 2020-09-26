bf731daeba18177b29b9c041842cd2b2d779a668
usaagi <usagi.com2@gmail.com>
2020-09-27 03:36:55 +0900

    一部ページで動作しない問題を修正(URLを追加)

--- kabu/KabutanDirectPDF.user.js
+++ kabu/KabutanDirectPDF.user.js
@@ -3,22 +3,21 @@
 // @description 株探内のPDFを従来のように直接表示します。
 // @namespace   usaagi
 // @include     https://kabutan.jp/stock/news?code=*
+// @include     https://kabutan.jp/stock/news/?code=*
 // @include     https://kabutan.jp/disclosures/*
 // @run-at      document-end
-// @version     0.3
+// @version     0.5
 // ==/UserScript==
 
-// a[target="pdf"]
-// TODO: https:// 化されたら href^="https://... にする
-let links = document.querySelectorAll('a[href*="://kabutan.jp/disclosures/pdf/"]');
+let links = document.querySelectorAll('a[href^="https://kabutan.jp/disclosures/pdf/"]');
 
 for (let i = 0; i < links.length; i++) {
     links[i].setAttribute('href', links[i].getAttribute('href').replace('/kabutan.jp/disclosures/pdf/', '/pdf.kabutan.jp/tdnet/data/').replace(/\/$/, '.pdf'));
 }
 
-
+// pdf iframeページでリダイレクトする方式
 // @include     http://kabutan.jp/disclosures/pdf/*/*/
 // @run-at      document-start
 /*
-location.replace(location.href.replace('/kabutan.jp/disclosures/pdf/', '/211.6.211.247/tdnet/data/').replace(/\/$/, '.pdf'));
+location.replace(location.href.replace('/kabutan.jp/disclosures/pdf/', '/pdf.kabutan.jp/tdnet/data/').replace(/\/$/, '.pdf'));
 */
