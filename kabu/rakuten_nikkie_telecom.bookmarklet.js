ca5060a7704bcedeef01ab4e220651df20f4c319
usaagi <8262083+usaagi@users.noreply.github.com>
2023-01-29 03:38:13 +0900

    Update rakuten_nikkie_telecom.bookmarklet.js

--- kabu/rakuten_nikkie_telecom.bookmarklet.js
+++ kabu/rakuten_nikkie_telecom.bookmarklet.js
@@ -1,8 +1,8 @@
 // 楽天証券から日経テレコンに移動するブックマークレット
 // 楽天証券にログイン後、URLにBV_SessionID=が含まれているページで実行可能
 //
-// 適当なブックマークを作り編集から以下のコード(1行)に変更。
-//javascript:(function(){var a=location.href.match("\\.do;BV_SessionID=(.*)\\?eventType=");a&&(location.href="https://member.rakuten-sec.co.jp/bv/app/info_jp_nikkei_telecom.do;BV_SessionID="+a[1]+"?eventType=init&agreeFlg=0");})();
+// 適当なブックマークを作り編集から以下のコード(1行)に変更。(javascript:からコピー)
+// javascript:(function(){var a=location.href.match("\\.do;BV_SessionID=(.*)\\?eventType=");a&&(location.href="https://member.rakuten-sec.co.jp/bv/app/info_jp_nikkei_telecom.do;BV_SessionID="+a[1]+"?eventType=init&agreeFlg=0");})();
 
 
 (function () {
