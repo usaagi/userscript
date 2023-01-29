08408bb6f30ac16e9800c9fe521e84baaef631dc
usaagi <8262083+usaagi@users.noreply.github.com>
2023-01-29 15:11:06 +0900

    セッションidの抽出方法を変更

--- kabu/rakuten_nikkie_telecom.bookmarklet.js
+++ kabu/rakuten_nikkie_telecom.bookmarklet.js
@@ -2,11 +2,11 @@
 // 楽天証券にログイン後、URLにBV_SessionID=が含まれているページで実行可能
 //
 // 適当なブックマークを作り編集から以下のコード(1行)に変更。(javascript:からコピー)
-// javascript:(function(){var a=location.href.match("\\.do;BV_SessionID=(.*)\\?eventType=");a&&(location.href="https://member.rakuten-sec.co.jp/bv/app/info_jp_nikkei_telecom.do;BV_SessionID="+a[1]+"?eventType=init&agreeFlg=0");})();
+// javascript:(function(){var a=location.href.match("\\.do;BV_SessionID=(.*)\\?");a&&(location.href="https://member.rakuten-sec.co.jp/bv/app/info_jp_nikkei_telecom.do;BV_SessionID="+a[1]+"?eventType=init&agreeFlg=0");})();
 
 
 (function () {
-    let m = location.href.match('\\.do;BV_SessionID=(.*)\\?eventType=');
+    let m = location.href.match('\\.do;BV_SessionID=(.*)\\?');
     if (m) {
         location.href = 'https://member.rakuten-sec.co.jp/bv/app/info_jp_nikkei_telecom.do;BV_SessionID=' + m[1] + '?eventType=init&agreeFlg=0'
     }
