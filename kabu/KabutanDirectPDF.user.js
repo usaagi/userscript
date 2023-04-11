d888e188e25a44ee92d0a9a293de44a76dccb015
usaagi <usagi.com2@gmail.com>
2023-04-11 18:35:20 +0900

    仕様変更に対応

--- kabu/KabutanDirectPDF.user.js
+++ kabu/KabutanDirectPDF.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        株探ダイレクトPDF
-// @description 株探内のPDFを従来のように直接表示します。
+// @description 株探内のPDFへのリンクを直接表示できるリンクに変換します。
 // @namespace   usaagi
 // @include     https://kabutan.jp/stock/news?code=*
 // @include     https://kabutan.jp/stock/news/?code=*
@@ -14,10 +14,3 @@ let links = document.querySelectorAll('a[href^="https://kabutan.jp/disclosures/p
 for (let i = 0; i < links.length; i++) {
     links[i].setAttribute('href', links[i].getAttribute('href').replace('/kabutan.jp/disclosures/pdf/', '/tdnet-pdf.kabutan.jp/').replace(/\/$/, '.pdf'));
 }
-
-// pdf iframeページでリダイレクトする方式
-// @include     http://kabutan.jp/disclosures/pdf/*/*/
-// @run-at      document-start
-/*
-location.replace(location.href.replace('/kabutan.jp/disclosures/pdf/', '/pdf.kabutan.jp/tdnet/data/').replace(/\/$/, '.pdf'));
-*/
