536c0f756b631cbdf2b9c92819541bcda101a720
usaagi <8262083+usaagi@users.noreply.github.com>
2024-03-11 08:26:49 +0900

    微修正

--- kabu/rakutenTelcon.user.js
+++ kabu/rakutenTelcon.user.js
@@ -1,26 +1,24 @@
 // ==UserScript==
-// @name        楽天
-// @description テレコンを普通のブラウザで利用
+// @name        楽天テレコン
+// @description 楽天証券のページ下部に日経テレコンへのリンクを追加
 // @namespace   usaagi
 // @include     https://member.rakuten-sec.co.jp/app/*.do;BV_SessionID=*
-// @version     0.3
+// @version     0.4
 // @run-at      document-end
 // ==/UserScript==
-
 
 'use strict';
 
 console.log(`テレコンのリンクを表示 ${location.href}`);
 
-let m = location.href.match(String.raw`/app/.*\.do;BV_SessionID=(.*)\?eventType=`);
+const m = location.href.match(String.raw`/app/.*\.do;BV_SessionID=(.*)\?eventType=`);
 
 if (m) {
-
     let session_id = m[1];
 
     console.log(`session id: ${session_id}`);
     //  console.dir(document.getElementById('siteID').getElementsByTagName('a')[0].getAttribute('href'));
-       
+
     let div = document.createElement('div');
     div.innerHTML = `<a href="https://member.rakuten-sec.co.jp/bv/app/info_jp_nikkei_telecom.do;BV_SessionID=${session_id}?eventType=init&agreeFlg=0" target="_blank">日経テレコンへ</a>`;
     div.setAttribute('style', 'padding: 10px; text-align: right;');
