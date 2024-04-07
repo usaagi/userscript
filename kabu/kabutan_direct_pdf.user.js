1b7fcba0368a2092c16cae0d5e173ab9caf795dd
usaagi <8262083+usaagi@users.noreply.github.com>
2024-04-08 00:14:49 +0900

    ファイル名の変更と微修正

---
+++ kabu/kabutan_direct_pdf.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name        株探ダイレクトPDF
+// @description 株探内のPDFへのリンクを直接表示できるリンクに変換します。
+// @namespace   usaagi
+// @match       https://kabutan.jp/stock/news?code=*
+// @match       https://kabutan.jp/stock/news/?code=*
+// @match       https://kabutan.jp/disclosures/*
+// @match       https://kabutan.jp/news/?&b=k*
+// @match       https://kabutan.jp/news/marketnews/?&b=k*
+// @run-at      document-end
+// @version     0.8
+// ==/UserScript==
+
+'use strict'
+
+let links = document.querySelectorAll('a[href^="https://kabutan.jp/disclosures/pdf/"]')
+
+for (let i = 0; i < links.length; i++) {
+    links[i].setAttribute('href', links[i].getAttribute('href').replace('/kabutan.jp/disclosures/pdf/', '/tdnet-pdf.kabutan.jp/').replace(/\/$/, '.pdf'))
+}
