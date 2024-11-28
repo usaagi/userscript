4e2377276e43d37769a0645e7d50bc1578618894
usaagi <8262083+usaagi@users.noreply.github.com>
2024-11-28 22:33:47 +0900

    更新

--- kabu/kabutan_direct_pdf.user.js
+++ kabu/kabutan_direct_pdf.user.js
@@ -4,17 +4,18 @@
 // @namespace   usaagi
 // @match       https://kabutan.jp/stock/news?code=*
 // @match       https://kabutan.jp/stock/news/?code=*
+// @match       https://kabutan.jp/stock/holder?code=*
 // @match       https://kabutan.jp/disclosures/*
 // @match       https://kabutan.jp/news/?&b=k*
 // @match       https://kabutan.jp/news/marketnews/?&b=k*
 // @run-at      document-end
-// @version     0.8
+// @version     1.1
 // ==/UserScript==
 
 'use strict'
 
-let links = document.querySelectorAll('a[href^="https://kabutan.jp/disclosures/pdf/"]')
+const links = document.querySelectorAll('a[href^="https://kabutan.jp/disclosures/pdf/"]')
 
 for (let i = 0; i < links.length; i++) {
-    links[i].setAttribute('href', links[i].getAttribute('href').replace('/kabutan.jp/disclosures/pdf/', '/tdnet-pdf.kabutan.jp/').replace(/\/$/, '.pdf'))
+	links[i].setAttribute('href', links[i].getAttribute('href').replace('/kabutan.jp/disclosures/pdf/', '/tdnet-pdf.kabutan.jp/').replace(/\/$/, '.pdf'))
 }
